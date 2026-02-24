6405574c35508337c77616b7325538730bd9084f
pabloor29 <pab.ortg@gmail.com>
2026-02-24 11:06:31 +0100

    docs: :memo: Create texte variables for language

--- constants/translations.ts
+++ constants/translations.ts
@@ -1,23 +1,96 @@
 export const t = {
   fr: {
-    welcome: 'Bienvenue',
-    myProducts: 'Mes produits',
-    noProduct: 'Aucun produit détecté...',
-    signOut: 'Se déconnecter',
-    editProfile: 'Modifier mes données',
-    save: 'Sauvegarder',
-    language: 'Langue',
-    theme: 'Thème',
-    // ... ajoute les clés dont tu as besoin
+    // Home
+    myProducts: "Mes produits",
+    noProduct: "Aucun produit détecté...",
+
+    // Edit and Add product
+    tileEditProduct: "Détails du produit",
+    creationDate: "Date de création",
+    productName: "Nom",
+    productAddress: "Adresse",
+    productZipCode: "Code postal",
+    productCity: "Ville",
+    productCountry: "Pays",
+    productCategory: "Catégorie",
+    productSecurityKey: "Clé de sécurité",
+    productHistoric: "Historique",
+
+    // Add page
+    QRcodeScanner: "QR code scanner",
+
+    // Profile
+    welcome: "Bienvenue",
+    language: "Langue",
+    theme: "Thème",
+    lightThemeLabel: "☀️ Clair",
+    darkThemeLabel: "🌙 Sombre",
+    systemThemeLabel: "📱 Système",
+    myInformationsLabel: "Mes informations",
+
+    // Profile informations
+    firstName: "Prénom",
+    lastName: "Nom",
+
+    // Buttons
+    saveButton: "Sauvegarder",
+    saveButtonPending: "Sauvegarde en cours...",
+    createButton: "Créer",
+    createButtonPending: "Création...",
+    returnButton: "Retour",
+    editProfileBUtton: "Modifier mon profil",
+    signOutButton: "Se déconnecter",
+
+    // Navigation bar
+    navHomeLabel: "Accueil",
+    navAddLabel: "Ajouter",
+    navProfileLabel: "Profil",
   },
   en: {
-    welcome: 'Welcome',
-    myProducts: 'My products',
-    noProduct: 'No product detected...',
-    signOut: 'Sign out',
-    editProfile: 'Edit my profile',
-    save: 'Save',
-    language: 'Language',
-    theme: 'Theme',
+    // Home
+    myProducts: "My products",
+    noProduct: "No product detected...",
+
+    // Edit and Add product
+    tileEditProduct: "Product details",
+    creationDate: "Creation date",
+    productName: "Name",
+    productAddress: "Address",
+    productZipCode: "Zip code",
+    productCity: "City",
+    productCountry: "Country",
+    productCategory: "Category",
+    productSecurityKey: "Security key",
+    productHistoric: "History",
+
+    // Add page
+    QRcodeScanner: "QR code scanner",
+
+    // Profile
+    welcome: "Welcome",
+    language: "Language",
+    theme: "Theme",
+    lightThemeLabel: "☀️ Light",
+    darkThemeLabel: "🌙 Dark",
+    systemThemeLabel: "📱 System",
+    myInformationsLabel: "My information",
+
+    // Profile informations
+    firstName: "First name",
+    lastName: "Last name",
+
+    // Buttons
+    saveButton: "Save",
+    saveButtonPending: "Saving...",
+    createButton: "Create",
+    createButtonPending: "Creating...",
+    returnButton: "Back",
+    editProfileBUtton: "Edit my profile",
+    signOutButton: "Sign out",
+
+    // Navigation bar
+    navHomeLabel: "Home",
+    navAddLabel: "Add",
+    navProfileLabel: "Profile",
   },
 };
